5ea411cdb2e719b963b59430f219785bcf119012
Jo-Zh <joannaweinazh@gmail.com>
2021-12-30 08:38:24 +1000

    first final-view

--- Quotes.js
+++ Quotes.js
@@ -1,7 +1,7 @@
 //quotes link: https://type.fit/api/quotes, source from freecodecamp forum
 $(document).ready(() => {
+   // get quotes
   const quoteMachine = () => {
-    //get quotes
     $.get("https://type.fit/api/quotes", (data) => {
       const quote = JSON.parse(data);
       const num = Math.ceil(Math.random() * (quote.length - 1));
@@ -9,7 +9,23 @@ $(document).ready(() => {
       $("#author").html(quote[num]["author"]);
     });
   };
-  //first load....
 
-  setInterval(quoteMachine, 5000);
+  //Load quotes....
+  const quoteSet = setInterval(quoteMachine, 5000);
+  quoteSet;
+
+  //stop showing quotes in small screen
+  $(window).on("resize", () => {
+    if ($(window).width() <= 767) {
+      clearInterval(quoteSet);
+    } else {
+      quoteSet;
+    }
+  });
+  // stop quotes by click close button
+  $("#close-quote").on("click", () => {
+    $("#quote-box").css("visibility", "hidden");
+    clearInterval(quoteSet);
+  });
+
 });
